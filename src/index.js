4c07e2c245920d48311c6a6aa46ea4b4888cffe0
NyoomNyoom <jacksonnorth1275@gmail.com>
2024-06-17 20:21:39 +1200

    Playing around, working how how things work

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const myelement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
+const myElement = (
+  <ul>
+    <li>Apples</li>
+    <li>Bananas</li>
+    <li>Lollies</li>
+  </ul>
 );
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
-root.render(myelement);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
